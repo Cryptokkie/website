a561635b552ca8058dfc2aacd9a1c7fb68e97850
Jeffrey Paarhuis <jpaarhuis@home.nl>
2019-03-20 12:14:18 +0100

    bugfix function

--- functions/posmn-coin-info/Services/CachingService.cs
+++ functions/posmn-coin-info/Services/CachingService.cs
@@ -1,7 +1,7 @@
+using Microsoft.Extensions.Caching.Memory;
 using posmn_coin_info.Models;
 using System;
 using System.Collections.Generic;
-using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,25 +9,17 @@ namespace posmn_coin_info.Services
 {
   public class CachingService
   {
-    private static readonly MemoryCache memoryCache = MemoryCache.Default;
+    private static readonly MemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
     private const string CurrenciesCacheKey = "currencies";
 
     public IEnumerable<CurrencyStats> GetCurrencies()
     {
-      CacheItem cacheContents = memoryCache.GetCacheItem(CurrenciesCacheKey);
-      return cacheContents?.Value as IEnumerable<CurrencyStats>;
+      return memoryCache.Get<IEnumerable<CurrencyStats>>(CurrenciesCacheKey);
     }
 
     public void StoreCurrencies(IEnumerable<CurrencyStats> currencies)
     {
-      CacheItem cacheContents = memoryCache.GetCacheItem(CurrenciesCacheKey);
-      CacheItemPolicy policy = new CacheItemPolicy
-      {
-        Priority = CacheItemPriority.Default,
-        AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(30)
-      };
-      cacheContents = new CacheItem(CurrenciesCacheKey, currencies);
-      memoryCache.Set(cacheContents, policy);
+      memoryCache.Set(CurrenciesCacheKey, currencies, DateTimeOffset.Now.AddMinutes(30));
     }
   }
 }
