8bc8f584126eb4a8dbb0bf4197a9d96bb23f1e26
Jeffrey Paarhuis <jpaarhuis@home.nl>
2019-05-06 00:03:18 +0200

    add function to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,9 @@ export const environment = {
     domain: 'posmn.eu.auth0.com',
     clientId: 'NZKRP75HXHVho2n55gyusGEmjiwW6HXa',
     callbackUrl: 'http://localhost:4200'
+  },
+  functions: {
+    // tslint:disable-next-line:max-line-length
+    deleteAccount: 'fill_me',
   }
 };
